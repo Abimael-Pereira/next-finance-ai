80b58284c823a0d273ae2b7ddf0f16dfbeb164db
Abimael <abimael.spdev@gmail.com>
2025-08-27 12:50:34 -0400

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-red-500">Home page</h1>;
+  return (
+    <div>
+      <h1 className="text-red-500">Home page</h1>
+      <Button>Alouu</Button>
+    </div>
+  );
 }
